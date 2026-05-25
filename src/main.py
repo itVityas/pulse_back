cdef823a0d98d45c71473a72ffb1cd495a7f4a64
Anton Kralin <kralin.dart@gmail.com>
2026-05-25 09:08:10 +0300

    change corps

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=project_config.backend_cors_origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
