1ac8e07ea4d8152ea8e05b21300f2110b16baad3
AntonKralin <kralin.dart@gmail.com>
2026-06-18 21:39:24 +0300

    add fileuploadschema

--- src/schema/file_upload.py
+++ src/schema/file_upload.py
@@ -1,9 +1,23 @@
-from datetime import date as idate
+from datetime import date as idate, datetime
 
 from pydantic import BaseModel
+from schema.currency import CurrencyFullSchema
+from schema.shop import ShopSmallSchema
 
 
 class FileUploadSchema(BaseModel):
     currency_id: int
     shop_id: int
     date: idate
+
+
+class FileUploadModelResponseSchema(BaseModel):
+    id: int
+    file_name: str
+    currency: CurrencyFullSchema
+    shop: ShopSmallSchema
+    date: idate
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
