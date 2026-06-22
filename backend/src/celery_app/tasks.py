8678ac9f4bb4cf8c3c326fb09bdb336d207969eb
Anton Kralin <kralin.dart@gmail.com>
2026-06-22 16:29:02 +0300

    temp

--- backend/src/celery_app/tasks.py
+++ backend/src/celery_app/tasks.py
@@ -1,7 +1,20 @@
-from celery_app.celery_app import celery_app
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
+from celery_app.celery_app import celery_app
+from settings.config import database_config
+from repository.currency import CurrencyData
 
 
 @celery_app.task
 def update_currency_rates_task():
-    print(1)
+    import asyncio
+
+    async def _run():
+        print(database_config.get_url())
+        engine = create_async_engine(database_config.get_url())
+        session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+        curerrensy = CurrencyData(session).get_multi()
+        for i in curerrensy:
+            print(i)
+
+    asyncio.run(_run())
