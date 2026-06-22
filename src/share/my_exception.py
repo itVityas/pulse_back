a336871b243209351b491e097e973d4f17a58cc0
Anton Kralin <kralin.dart@gmail.com>
2026-06-22 10:52:13 +0300

    remove unnessesary

--- src/share/my_exception.py
+++
@@ -1,5 +0,0 @@
-class MyHttpException(Exception):
-    def __init__(self, status_code: int, detail: str, title: str):
-        self.title = title
-        self.detail = detail
-        self.status_code = status_code
